7d9f3c40bd45a3983af7e77d3aff3d2adb59bb6b
Ribeironathalia <130074144+Ribeironathalia@users.noreply.github.com>
2024-09-03 08:19:13 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -9,11 +9,11 @@ const perguntas = [
         enunciado: "Qual é o principal objetivo do estilo de vida clean girl?"
         alternativas: [
             {
-                texto: Seguir tendências de moda rapidamente
+                texto: "Seguir tendências de moda rapidamente"
                 afirmacao: "terecoteco"
             },
             {
-                texto: Manter uma rotina de beleza e cuidados com a saúde de forma simples e minimalista
+                texto: "Manter uma rotina de beleza e cuidados com a saúde de forma simples e minimalista"
                 afirmacao: "Quis saber como usar IA no seu dia a dia."
             }
         ]
@@ -23,11 +23,11 @@ const perguntas = [
     enunciado: "O que é geralmente enfatizado no visual clean girl?"
     alternativas:[
         {
-            texto: Maquiagem ousada e cores vibrantes
+            texto: "Maquiagem ousada e cores vibrantes"
             afirmacao:
         },
         {
-            texto: Pele natural e maquiagem leve
+            texto: "Pele natural e maquiagem leve"
             afirmacao: 
            
         },
@@ -35,15 +35,15 @@ const perguntas = [
 },
 
 {
-    enunciado: Qual é um dos itens essenciais no guarda-roupa de uma clean girl?
+    enunciado: "Qual é um dos itens essenciais no guarda-roupa de uma clean girl?"
     alternativas:[
         {
-            texto: Roupas com estampas extravagantes
+            texto: "Roupas com estampas extravagantes"
             afirmacao: 
            
         },
         {
-            texto: Peças básicas e atemporais
+            texto: "Peças básicas e atemporais"
             afirmacao: 
            
         },
@@ -51,31 +51,31 @@ const perguntas = [
 },
 
 {
-    enunciado: "Qual é a abordagem típica da clean girl em relação à alimentação?",
+    enunciado: "Você se considera uma Clean Girl?"
     alternativas:[
         {
-            texto: 
-            afirmacao: "Afirmação 1a alternativa"          
+            texto: Sim, sempre
+            afirmação:           
            
         },
         {
-            texto: "B) Permite carregar mais peso e aumentar a força dos quadríceps e glúteos",
-            afirmacao: "Afirmação 2a alternativa"
+            texto: "Não, não sou muito fã",
+            afirmacao: 
            
         },
     ]
 },
 
 {
-    enunciado: "Qual é o benefício de realizar exercícios de alongamento antes do treino de pernas?",
+    enunciado: "Você faz skincare todo dia?",
     alternativas:[
         {
-            texto: "A) Aumenta a flexibilidade e reduz o risco de lesões",
+            texto: "Sim, todos os dias",
             afirmacao: "Afirmação 1a alternativa"
            
         },
         {
-            texto: "B) Aumenta a massa muscular diretamente",
+            texto: "Não muito, não tenho esse costume",
             afirmacao: "Afirmação 2a alternativa"
          
         },
@@ -103,4 +103,27 @@ function mostraAlternativas(){
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
+mostraPergunta();
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    caixaAlternativas.textContent="";
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntas[atual].enunciado;
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", function(){
+            atual++;
+            mostraPergunta();
+        });        
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
 mostraPergunta();
